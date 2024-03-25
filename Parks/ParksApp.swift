e259f45560634c1f67812c0c782b4f7283ca4c82
Shantalia <71754706+Stp155906@users.noreply.github.com>
2024-03-25 11:54:25 -0400

    Initial Commit

---
+++ Parks/ParksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParksApp.swift
+//  Parks
+//
+//  Created by Shantalia Z on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ParksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
