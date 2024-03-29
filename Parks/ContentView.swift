c58bab3ca245a5c9213764e20c044186217abbf7
Shantalia <71754706+Stp155906@users.noreply.github.com>
2024-03-29 16:56:36 -0400

    update
    update

--- Parks/ContentView.swift
+++ Parks/ContentView.swift
@@ -4,18 +4,66 @@
 //
 //  Created by Shantalia Z on 3/25/24.
 //
+// AI Translator app
+//let urlString = "https://developer.nps.gov/api/v1/parks?stateCode=wa&api_key=hijjfJdhmPG8HghuZX8u4nac1CizfwRYgThBFQEz"
+
+
 
 import SwiftUI
 
+
 struct ContentView: View {
+
+    @State private var parks: [Park] = []
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                    LazyVStack {
+                        ForEach(parks) { park in
+                            NavigationLink(value: park) {
+                                ParkRow(park: park)
+                            }
+                        }
+                    }
+                }
+            
+                
+            .padding()
+            .onAppear(perform: {
+                Task {
+                    await fetchParks()
+                }
+        })
         }
-        .padding()
+    }
+
+    private func fetchParks() async {
+        // URL for the API endpoint
+        // 👋👋👋 Make sure to replace {YOUR_API_KEY} in the URL with your actual NPS API Key
+        let url = URL(string: "https://developer.nps.gov/api/v1/parks?stateCode=wa&api_key=hijjfJdhmPG8HghuZX8u4nac1CizfwRYgThBFQEz")!
+        do {
+
+            // Perform an asynchronous data request | make request
+            let (data, _) = try await URLSession.shared.data(from: url)
+
+            // Decode json data into ParksResponse type | convert data
+            let parksResponse = try JSONDecoder().decode(ParksResponse.self, from: data)
+
+            // Get the array of parks from the response | create array variable
+            let parks = parksResponse.data
+
+            // Print the full name of each park in the array | print each name in the array
+            for park in parks {
+                print(park.fullName)
+            }
+
+            // Set the parks state property | state 
+            self.parks = parks
+
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
 
---
+++ Parks/ParksResponse.swift
@@ -0,0 +1,43 @@
+//
+//  ParksResponse.swift
+//  Parks
+//
+//  Created by Shantalia Z on 3/29/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct ParksResponse: Codable{
+    let data: [Park]
+    
+}
+
+struct Park: Codable, Identifiable, Hashable, Equatable {
+//    static func == (lhs: Park, rhs: Park) -> Bool {
+//        <#code#>
+//    }
+    
+    
+    let id : String
+    let fullName: String
+    let description: String
+    let latitude: String
+    let longitude: String
+    let images: [ParkImage]
+    let name: String
+    
+    //added
+    func hash(into hasher: inout Hasher) { // <-- Add required hash function
+            hasher.combine(id)
+        }
+}
+struct ParkImage: Codable, Identifiable{
+    let title: String
+    let caption : String
+    let url: String
+    // added for identifiable protocol
+    var id: String { // <-- Add id property to conform to Identifiable
+            return url // <-- Use the url string as t he id since it will be unique for a given image
+        }
+}
